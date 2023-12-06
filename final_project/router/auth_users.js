3216852ca74641ebbc13905a0feca87d2a333bf9
Josh Martin <45828392+joshualmartin@users.noreply.github.com>
2023-12-06 16:20:33 -0500

    Add files via upload

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -5,24 +5,49 @@ const regd_users = express.Router();
 
 let users = [];
 
-const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
-}
+const jwtSecret = 'your_jwt_secret'; // This should be stored securely and not hardcoded
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
-}
+// Function to check if the username is valid
+const isValid = (username) => {
+    // Example validation: check if username exists in the users array
+    return users.some(user => user.username === username);
+};
 
-//only registered users can login
-regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+// Function to check if username and password match the one we have in records
+const authenticatedUser = (username, password) => {
+    return users.some(user => user.username === username && user.password === password);
+};
+
+// Login route for registered users
+regd_users.post("/login", (req, res) => {
+    const { username, password } = req.body;
+    if (authenticatedUser(username, password)) {
+        const token = jwt.sign({ username }, jwtSecret, { expiresIn: '1h' });
+        return res.status(200).json({ token });
+    } else {
+        return res.status(401).json({ message: "Invalid credentials" });
+    }
 });
 
-// Add a book review
+// Add or modify a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    const review = req.body.review;
+    const username = req.user.username; // Assuming username is stored in JWT payload
+
+    const book = books.find(b => b.isbn === isbn);
+    if (!book) {
+        return res.status(404).json({ message: "Book not found" });
+    }
+
+    const userReview = book.reviews.find(r => r.username === username);
+    if (userReview) {
+        userReview.review = review;
+    } else {
+        book.reviews.push({ username, review });
+    }
+
+    return res.status(200).json({ message: "Review added/updated successfully" });
 });
 
 module.exports.authenticated = regd_users;
--- final_project/router/booksdb.js
+++ final_project/router/booksdb.js
@@ -1,14 +1,14 @@
 let books = {
-      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {} },
-      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {} },
-      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {} },
-      4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {} },
-      5: {"author": "Unknown","title": "The Book Of Job", "reviews": {} },
-      6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {} },
-      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {} },
-      8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {} },
-      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {} },
-      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {} }
+      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": "", "isbn": "123456789" },
+      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": "Great book!", "isbn": "223456789" },
+      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": "Great book!", "isbn": "323456789" },
+      4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": "Great book!", "isbn": "423456789" },
+      5: {"author": "Unknown","title": "The Book Of Job", "reviews": "Great book!", "isbn": "523456789" },
+      6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": "Great book!", "isbn": "623456789" },
+      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": "Great book!", "isbn": "723456789" },
+      8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": "Great book!", "isbn": "823456789" },
+      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": "Great book!", "isbn": "923456789" },
+      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": "Great book!", "isbn": "023456789" }
 }
 
 module.exports=books;
